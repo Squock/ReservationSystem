323dfcc96c7cf4d6c871da6f594f9128c15f05bf
Айаал Неустроев < >
2016-11-17 16:33:11 +0900

    Добавил миграцию

---
+++ migrate.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from app import app
+#documentation Flask-Migrate https://flask-migrate.readthedocs.io/en/latest/
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/dataBaseSite'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
